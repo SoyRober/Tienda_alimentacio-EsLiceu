37e7040cf72a69b1b1799d6819bb934282a4ef4c
DaruPlays <xavipanizamartinez@gmail.com>
2021-04-29 10:56:26 +0200

    0.0.2

---
+++ OK.php
@@ -0,0 +1,8 @@
+<?php
+require "includes/head.php";
+require "includes/header.php";
+?>
+<body>
+echo "TODO OKI DOKI" 
+</body>
+
---
+++ error.php
@@ -0,0 +1,9 @@
+<?php
+require "includes/head.php";
+require "includes/header.php";
+?>
+<body>
+echo "CAGASTE MI REY" 
+</body>
+
+
